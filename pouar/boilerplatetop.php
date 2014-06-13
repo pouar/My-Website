1a86e3aef4d169a0a980382927a34ded2803eedd
Pouar <thepouar@gmail.com>
2014-06-13 16:52:38 -0500

    Eye's mistaken 2 single quotes for a double quote.

--- pouar/boilerplatetop.php
+++ pouar/boilerplatetop.php
@@ -11,7 +11,7 @@ echo <<<EOF
 <a href="https://github.com/pouar/My-Website"><img style="position: absolute; top: 0; right: 0; border: 0;" src="forkmebanner.png" alt="Fork me on GitHub"></a>
 EOF;
 echo '<link rel="stylesheet" type="text/css" href="style.css">';
-echo '<link rel="icon" type="image/png" href=''marioskate.png" />';
+echo '<link rel="icon" type="image/png" href="marioskate.png" />';
 echo '</head><body>';
 echo '<div id=top><a href='.$root.'><img src=marioskatehq2x.png style="position:absolute;top:0;left:0;"></a><div style="position:absolute;top:0px;right:150px;left:50px;"><ul id=links>';
 foreach($toplinks as list($i, $j))
