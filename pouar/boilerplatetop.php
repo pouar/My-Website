cf6c50181642564f44ce88bef5fe6cb2adafb7cb
Pouar <thepouar@gmail.com>
2014-06-16 08:18:30 -0500

    add forum link

--- pouar/boilerplatetop.php
+++ pouar/boilerplatetop.php
@@ -1,11 +1,17 @@
 <?hh
-$root='http://'.$_SERVER['HTTP_HOST'].'/pouar/';
+if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+	$root='http://'.$_SERVER['HTTP_HOST'].'/pouar/';
+}
+else {
+	$root='http://'.$_SERVER['HTTP_HOST'].'/pouar/';
+}
 $toplinks = [
 ["blog.php", "Blog"],
 ["downloads", "Downloads"],
 ["form.php", "Joke form"],
 ["3dlogo.php", "3D logo in three.js"],
 ["../bugzilla", "Bug Tracker"],
+["../forum", "forum"],
 ];
 echo <<<EOF
 <a href="https://github.com/pouar/My-Website"><img style="position: absolute; top: 0; right: 0; border: 0;" src="forkmebanner.png" alt="Fork me on GitHub"></a>
@@ -13,7 +19,7 @@ EOF;
 echo '<link rel="stylesheet" type="text/css" href="style.css">';
 echo '<link rel="icon" type="image/png" href="marioskate.png" />';
 echo '</head><body>';
-echo '<div id=top><a href='.$root.'><img src=marioskatehq2x.png style="position:absolute;top:0;left:0;"></a><div style="position:absolute;top:0px;right:150px;left:50px;"><ul id=links>';
+echo '<div id=top><a href=/pouar/><img src=marioskatehq2x.png style="position:absolute;top:0;left:0;"></a><div style="position:absolute;top:0px;right:150px;left:50px;"><ul id=links>';
 foreach($toplinks as list($i, $j))
 {
 	echo '<li><a class=toplink href="'.$i.'">'.$j.'</a></li>';
