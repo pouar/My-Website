691dabb1b0768b603f2e1057ed2b833bf99453a4
Pouar <thepouar@gmail.com>
2014-06-19 16:59:53 -0500

    formating for directory listing

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?hh
 include_once $_SERVER['DOCUMENT_ROOT'].'pouar/boilerplatetop.php';
 $files = scandir('.');
+echo "<div id=news style=\"position:relative;margin:50px;\"><h3 style=\"display:inline\">".$_SERVER['REQUEST_URI']."</h3><hr>".php_uname();
 foreach($files as $i)
 {
 	echo '<p><a href="'.$i.'">'.$i.'</a></p>';
