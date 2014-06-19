305509540139f4847739f361b10799520fa40eb7
Pouar <thepouar@gmail.com>
2014-06-19 16:41:43 -0500

    use direct links for rss

--- pouar/blog.php
+++ pouar/blog.php
@@ -1,9 +1,7 @@
 <?hh
 echo '<!doctype html><html><head><title>Blog</title>';
 include_once "boilerplatetop.php";
-ob_start();
-include 'blogfeed.php';
-$news = new SimpleXMLElement(ob_get_clean());
+$news = simplexml_load_file('blogfeed.php');
 echo "<div id=news style=\"position:relative;margin:50px;\"><h3 style=\"display:inline\">".$news->channel->title."</h3><a href=blogfeed.php style=\"display:inline;right:0px;position:absolute;\"><img src=Rss-icon.png></a><hr>".php_uname();
 for($i=0;$i<5;$i++)
 {
--- pouar/index.php
+++ pouar/index.php
@@ -1,9 +1,7 @@
 <?hh
 echo '<!doctype html><html><head><title>My new website</title>';
 include_once "boilerplatetop.php";
-ob_start();
-include 'news.php';
-$news = new SimpleXMLElement(ob_get_clean());
+$news = simplexml_load_file('news.php');
 echo "<div id=news style=\"position:relative;margin:50px;\"><h3 style=\"display:inline\">".$news->channel->title."</h3><a href=news.php style=\"display:inline;right:0px;position:absolute;\"><img src=Rss-icon.png></a><hr>".php_uname();
 for($i=0;$i<5;$i++)
 {
