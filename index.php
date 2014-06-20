81aea5eab683e7a90f26b16b1db7498f530902b3
Pouar <thepouar@gmail.com>
2014-06-19 19:06:58 -0500

    add more info in file listing

--- index.php
+++ index.php
@@ -2,8 +2,19 @@
 include_once $_SERVER['DOCUMENT_ROOT'].'pouar/boilerplatetop.php';
 $files = scandir('.');
 echo "<div id=news style=\"position:relative;margin:50px;\"><h3 style=\"display:inline\">".$_SERVER['REQUEST_URI']."</h3><hr>".php_uname();
+echo <<<EOF
+<style>
+.td
+{
+	width:33%;
+}
+</style>
+EOF;
+echo '<table style="width:100%">';
+	echo '<tr><th class=td>file</th><th class=td>file size</th><th class=td>mtime</th></tr>';
 foreach($files as $i)
 {
-	echo '<p><a href="'.$i.'">'.$i.'</a></p>';
+	echo '<tr><td class=td><a href="'.$i.'">'.$i.'</a></td><td class=td>'.(filesize($i)/1024).' kb</td><td class=td>'.date('r',filemtime($i)).'</td></tr>';
 }
+echo '</table></div>';
 include_once $_SERVER['DOCUMENT_ROOT'].'pouar/boilerplatebot.php';
