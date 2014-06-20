918cad8b38fd73dc4385588dce4f3e9620e16a8a
Pouar <thepouar@gmail.com>
2014-06-19 20:09:46 -0500

    another error

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?hh
 echo '<!doctype html><html><head><title>My new website</title>';
-include_once $_SERVER['DOCUMENT_ROOT'].'pouar/boilerplatetop.php';
-$files = scandir('.');
-echo "<div id=news style=\"position:relative;margin:50px;\"><h3 style=\"display:inline\">".$_SERVER['REQUEST_URI']."</h3><hr>".php_uname();
 echo <<<EOF
 <style>
 .td
@@ -11,11 +8,14 @@ echo <<<EOF
 }
 </style>
 EOF;
+include_once $_SERVER['DOCUMENT_ROOT'].'pouar/boilerplatetop.php';
+$files = scandir('.');
+echo "<div id=news style=\"position:relative;margin:50px;\"><h3 style=\"display:inline\">".$_SERVER['REQUEST_URI']."</h3><hr>".php_uname();
 echo '<table style="width:100%">';
 	echo '<tr><th class=td>file</th><th class=td>file size</th><th class=td>mtime</th></tr>';
 foreach($files as $i)
 {
-	echo '<tr><td class=td><a href="'.$i.'">'.$i.'</a></td><td class=td>';
+	echo '<tr><td class=td><a href="'.rawurlencode($i).'">'.$i.'</a></td><td class=td>';
 	$filesize=filesize($i);
 	if($filesize>=1024000000)
 	{
