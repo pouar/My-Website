cede715f676c839227d2ee8f7fdce469a2a6bc43
Pouar <thepouar@gmail.com>
2014-07-04 09:37:14 -0500

    typo

--- pouar/boilerplatebot.php
+++ pouar/boilerplatebot.php
@@ -3,8 +3,8 @@ $poweredby = [
 ["http://www.nginx.org ", "/pouar/nginx-logo.png"],
 ["http://hhvm.com", "/pouar/hhvm.png"],
 ["http://hacklang.org", "/pouar/hack.png"],
-["href=http://www.php.net", "/pouar/220px-PHP-logo.svg.png"],
-["href=http://www.centos.org", "/pouar/centos-logo-light.png"],
+["http://www.php.net", "/pouar/220px-PHP-logo.svg.png"],
+["http://www.centos.org", "/pouar/centos-logo-light.png"],
 ];
 echo "
 				</td>
